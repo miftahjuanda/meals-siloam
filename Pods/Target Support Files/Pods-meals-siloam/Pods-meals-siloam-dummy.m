90d331b8b40c6b1ce743b3e7937a5ca0f1cf0b9d
Miftah Juanda <miftahjuanda21@gmail.com>
2023-10-20 17:04:13 +0700

    add cocoapods and setup project

---
+++ Pods/Target Support Files/Pods-meals-siloam/Pods-meals-siloam-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_meals_siloam : NSObject
+@end
+@implementation PodsDummy_Pods_meals_siloam
+@end
---
+++ meals-siloam/Applications/SceneDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  SceneDelegate.swift
+//  meals-siloam
+//
+//  Created by Miftah Juanda Batubara on 20/10/23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.backgroundColor = UIColor.white
+        
+        if let window = window {
+            let mainVC = ViewController()
+            let navigationController = UINavigationController.init(rootViewController: mainVC)
+            navigationController.navigationBar.tintColor = .black
+            window.rootViewController = navigationController
+        }
+        window?.overrideUserInterfaceStyle = .light
+        window?.windowScene = windowScene
+        window?.makeKeyAndVisible()
+    }
+}
+
